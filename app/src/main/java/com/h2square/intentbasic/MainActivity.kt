0070c76ec00e916e5cfdd809908cb877ebaf22ba
jungbin-lee <hihihi486@naver.com>
2023-05-08 21:36:45 +0900

    practice DIAL

--- app/src/main/java/com/h2square/intentbasic/MainActivity.kt
+++ app/src/main/java/com/h2square/intentbasic/MainActivity.kt
@@ -2,6 +2,7 @@ package com.h2square.intentbasic
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,6 +12,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        dialBtn.setOnClickListener{
+            val inputPhoneNum = phoneNumEdt.text.toString()
+            val myUri = Uri.parse("tel:${inputPhoneNum}")
+
+            val myIntent =  Intent(Intent.ACTION_DIAL, myUri)
+
+            startActivity(myIntent)
+
+        }
 
 
         editNickNameBtn.setOnClickListener {
